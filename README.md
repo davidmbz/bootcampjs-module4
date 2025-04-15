46ca7941bb8b4709c824cec919565965e92595a3
David Martín <dmartinblazquez@gmail.com>
2025-04-15 18:06:16 +0200

    Implementación funcionalidades

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bootcampjs-module4
+
 Laboratorio del Módulo 4 del BootcampJS de Lemoncode
+
+- Creación del proyecto
+- Maquetación del display en HTML y CSS
+- Implementación de las funcionalidades
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,63 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+// VARIABLES
+let turno: number = 0;
+
+const numeroTurno = document.getElementById("numero-turno");
+const btnAnterior = document.getElementById("btn-anterior");
+const btnSiguiente = document.getElementById("btn-siguiente");
+const btnReset = document.getElementById("btn-reset");
+const btnTurno = document.getElementById("btn-turno");
+const inputTurno = document.getElementById("input-turno");
+
+// DISPLAY TURNOS
+function actualizarDisplay(): void {
+  if (numeroTurno) {
+    numeroTurno.textContent = turno.toString().padStart(2, "0");
+  }
+}
+
+// BOTÓN TURNO ANTERIOR
+if (btnAnterior instanceof HTMLButtonElement) {
+  btnAnterior.addEventListener("click", () => {
+    if (turno > 0) {
+      turno--;
+      actualizarDisplay();
+    }
+  });
+}
+
+// BOTÓN TURNO SIGUIENTE
+if (btnSiguiente instanceof HTMLButtonElement) {
+  btnSiguiente.addEventListener("click", () => {
+    turno++;
+    actualizarDisplay();
+  });
+}
+
+// BOTÓN RESET
+if (btnReset instanceof HTMLButtonElement) {
+  btnReset.addEventListener("click", () => {
+    turno = 0;
+    actualizarDisplay();
+  });
+}
+
+// TURNO MANUAL
+if (
+  btnTurno instanceof HTMLButtonElement &&
+  inputTurno instanceof HTMLInputElement
+) {
+  btnTurno.addEventListener("click", () => {
+    const nuevoTurno = parseInt(inputTurno.value, 10);
+    if (typeof nuevoTurno === "number" && nuevoTurno >= 0) {
+      turno = nuevoTurno;
+      actualizarDisplay();
+    } else {
+      alert("Introduce un número válido mayor o igual a 0");
+    }
+  });
+}
+
+// INICIALIZAMOS
+actualizarDisplay();
